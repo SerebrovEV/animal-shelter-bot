74b90c9ded765f5b64f656644e15a45debae7a54
SerebrovEV <89818121788@mail.ru>
2022-12-15 15:20:24 +0300

    Первый запуск мастер ветки 15.12.2022

---
+++ src/test/java/com/animalshelter/animalshelterbot/AnimalShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalshelter.animalshelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
