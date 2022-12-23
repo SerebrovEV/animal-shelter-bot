daacffa32d97add04655c00b8b8a88d7cdd6937c
SerebrovEV <89818121788@mail.ru>
2022-12-23 09:16:37 +0300

    Добавление аннотации из Lombok в BotUser.java

--- src/main/java/com/animalshelter/animalshelterbot/model/BotUser.java
+++ src/main/java/com/animalshelter/animalshelterbot/model/BotUser.java
@@ -1,10 +1,13 @@
 package com.animalshelter.animalshelterbot.model;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@RequiredArgsConstructor
 @Entity
 public class BotUser {
     @Id
@@ -24,37 +27,6 @@ public class BotUser {
         this.userId = userId;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
     @Override
     public String toString() {
