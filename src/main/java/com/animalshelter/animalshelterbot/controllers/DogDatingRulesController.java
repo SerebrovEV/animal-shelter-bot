fdc4d450b70e0d97cc1953a1a42d896384662b48
b.m.ulyashev <b.m.ulyashev@mail.ru>
2023-01-09 22:55:45 +0300

    Добавлен контроллер для рекомендациям по знакомству и тесты.

--- src/main/java/com/animalshelter/animalshelterbot/controllers/DogDatingRulesController.java
+++ src/main/java/com/animalshelter/animalshelterbot/controllers/DogDatingRulesController.java
@@ -1,13 +1,19 @@
 package com.animalshelter.animalshelterbot.controllers;
 
 import com.animalshelter.animalshelterbot.handler.Callback;
+import com.animalshelter.animalshelterbot.handler.Command;
 import com.animalshelter.animalshelterbot.handler.CommandController;
 import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * <i>Контроллер получения информации по правилам знакомства с собакой.</i>
@@ -18,29 +24,24 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DogDatingRulesController implements CommandController {
 
-
-    private final String dogDatingRulesText =
-            "*Правила знакомства с собакой:*\n\n"
-                    + "  *Что нельзя делать:*\n"
-                    + "    - Не нависайте над собакой (не наклоняйтесь над ней);\n"
-                    + "    - Не нужно смотреть пристально собаке в глаза;\n"
-                    + "    - Не нужно обнимать и прижимать к себе собаку;\n"
-                    + "    - Не нужно целовать ее;\n"
-                    + "    - Не трогайте за голову, и не гладьте по голове;\n"
-                    + "    - Не надо кричать, визжать...;\n"
-                    + "    - Не нужно приближаться быстро и лоб в лоб;\n\n"
-                    + "  *Как правильно:*\n"
-                    + "    - Дайте собаке возможность самой подойти к вам. Если вам нужно подойти к ней, делайте это немного как бы по дуге и не торопясь.\n"
-                    + "    - Дайте собаке возможность обнюхать вас. Если она сильно боится, можно присесть на корточки, но, не наклоняясь над ней (если собачка маленькая).\n"
-                    + "    - Избегайте взгляда глаза в глаза.\n"
-                    + "    - Можете погладить по бокам, щечкам, груди, если она не сопротивляется — по спине.\n"
-                    + "    - Если волонтер не против, можете угостить лакомством на открытой ладони.\n";
+    private final String pathToFileRecommendation = "src/main/resources/textinfo/dog_dating_rules_recommendation.txt";
 
     public static final String DOG_DATING_RULES_CALLBACK = "dogDatingRules";
 
+    /**
+     *Для проверки
+     */
+    @Command(name = "/rules")
+    public SendMessage handleDescriptionMessage(Message message) throws IOException {
+        String text = Files.readString(Paths.get(pathToFileRecommendation));
+        return new SendMessage(message.from().id(), text)
+                .parseMode(ParseMode.Markdown);
+    }
+
     @Callback(name = DOG_DATING_RULES_CALLBACK)
-    public SendMessage handleCallbackMessage(CallbackQuery callbackQuery) {
-        return new SendMessage(callbackQuery.from().id(), dogDatingRulesText)
+    public SendMessage handleCallbackMessage(CallbackQuery callbackQuery) throws IOException {
+        String text = Files.readString(Paths.get(pathToFileRecommendation));
+        return new SendMessage(callbackQuery.from().id(), text)
                 .parseMode(ParseMode.Markdown);
     }
 }
