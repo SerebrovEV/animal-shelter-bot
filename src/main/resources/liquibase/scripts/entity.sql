48b0e03ba7f205c3f1edac6bca344865eeabd3f3
MaraOD555 <ruffbc@hotmail.com>
2023-01-11 23:17:54 +0300

    добавлены сущности собаки и кошки

--- src/main/resources/liquibase/scripts/entity.sql
+++ src/main/resources/liquibase/scripts/entity.sql
@@ -2,9 +2,22 @@
 
 --changeset sev:1
 create table bot_user(
-
     id            bigserial,
     chat_id       bigint,
     user_name text,
     phone_number  bigint
 );
+--changeset mara:1
+create table adopted_dog(
+   id             BIGSERIAL,
+   dog_name       TEXT,
+   adoption_date  DATE,
+   trial_period   INTEGER
+);
+--changeset mara:2
+ create table adopted_cat(
+     id             BIGSERIAL,
+     cat_name       TEXT,
+     adoption_date  DATE,
+     trial_period   INTEGER
+ );
