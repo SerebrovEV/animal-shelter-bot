98c52b527723524213d02603833e42491cabcb6a
MaraOD555 <ruffbc@hotmail.com>
2023-01-13 22:26:51 +0300

    настроена связка таблиц юзеров и питомцев

--- src/main/resources/liquibase/scripts/entity.sql
+++ src/main/resources/liquibase/scripts/entity.sql
@@ -3,7 +3,7 @@
 --changeset sev:1
 create table dog_user(
 
-    id            bigserial,
+    id            bigserial PRIMARY KEY,
     chat_id       bigint,
     user_name text,
     phone_number  bigint
@@ -11,22 +11,29 @@ create table dog_user(
 --changeset sev:2
 create table cat_user(
 
-    id            bigserial,
+    id            bigserial PRIMARY KEY ,
     chat_id       bigint,
     user_name text,
     phone_number  bigint
 );
 --changeset mara:1
 create table adopted_dog(
-   id             BIGSERIAL,
+   id             BIGSERIAL PRIMARY KEY ,
    dog_name       TEXT,
    adoption_date  DATE,
-   trial_period   INTEGER
+   trial_period   INTEGER DEFAULT '30'
 );
 --changeset mara:2
  create table adopted_cat(
-     id             BIGSERIAL,
+     id             BIGSERIAL PRIMARY KEY ,
      cat_name       TEXT,
      adoption_date  DATE,
-     trial_period   INTEGER
+     trial_period   INTEGER DEFAULT '30'
  );
+--changeset mara:3
+ALTER TABLE adopted_dog
+ADD COLUMN dog_user_id BIGINT REFERENCES adopted_dog(id);
+
+--changeset mara:4
+ALTER TABLE adopted_cat
+    ADD COLUMN cat_user_id BIGINT REFERENCES adopted_cat(id);
