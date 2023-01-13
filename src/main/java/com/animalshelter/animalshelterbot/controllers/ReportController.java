3261f575d2614e02fd1174f8ebc15227e5341186
SerebrovEV <89818121788@mail.ru>
2023-01-13 16:50:39 +0300

    Наработки

--- src/main/java/com/animalshelter/animalshelterbot/controllers/ReportController.java
+++ src/main/java/com/animalshelter/animalshelterbot/controllers/ReportController.java
@@ -2,15 +2,21 @@ package com.animalshelter.animalshelterbot.controllers;
 
 import com.animalshelter.animalshelterbot.handler.Command;
 import com.animalshelter.animalshelterbot.handler.CommandController;
+import com.animalshelter.animalshelterbot.model.DogReport;
 import com.animalshelter.animalshelterbot.repository.ReportRepository;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.PhotoSize;
+import com.pengrad.telegrambot.request.GetFile;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetFileResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
@@ -21,14 +27,14 @@ public class ReportController implements CommandController {
 
     private static final String addReport = "([\\W]{5})(\\s)([\\W]+)";
 
-    private  final Pattern pattern = Pattern.compile("([\\W]{5})(\\s)([\\W]+)");
+    private final Pattern pattern = Pattern.compile("Отчет ([\\W]+)");
     private final Logger LOG = LoggerFactory.getLogger(ReportController.class);
 
     ReportRepository reportRepository;
 
-    @Command(pattern = addReport)
-    public SendMessage addReport(Message message) {
-        long idUser = message.from().id();
+   // @Command(pattern = addReport)
+    public SendMessage addReport(String text, PhotoSize[] photoSizes, long idUser) {
+
         LOG.info("Пользователь {} направил отчет для записи в БД", idUser);
 //        byte[] photo = new byte[0];
 //        PhotoSize[] photoSizes = message.photo();
@@ -39,16 +45,15 @@ public class ReportController implements CommandController {
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
-//        }
-//        Report report = new Report();
-//        Matcher matcher = pattern.matcher(message.text());
-//        matcher.find();
-//        report.setPhoto(photo);
-//        report.setText(matcher.group(3));
-//        reportRepository.save(report);
+        //     }
+    //    DogReport report = new DogReport();
+     //   Matcher matcher = pattern.matcher(message.text());
+      //  matcher.find();
+        //report.setPhoto(photo);
+     //   report.setText(matcher.group(3));
+      //  reportRepository.save(report);
         return new SendMessage(idUser, "Test");
     }
-
 
 
 }
