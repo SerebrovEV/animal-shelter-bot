55edb4aa5416e5306356fe527c7143c537ace38c
b.m.ulyashev <b.m.ulyashev@mail.ru>
2023-01-18 19:05:51 +0300

    Добавлен help.

--- src/main/java/com/animalshelter/animalshelterbot/controllers/CatReportController.java
+++ src/main/java/com/animalshelter/animalshelterbot/controllers/CatReportController.java
@@ -7,6 +7,7 @@ import com.animalshelter.animalshelterbot.organisation.Callbacks;
 import com.animalshelter.animalshelterbot.service.CatReportService;
 import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -28,7 +29,19 @@ public class CatReportController implements CommandController {
     private final String DELETE_CAT_FROM_REPORT_BY_ID = "/deleteCatReportId ([\\d]+)";
     private final String GET_CATS_FROM_BAD_USERS = "/getBadUsers (\\d{4}-\\d{2}-\\d{2})";
     private final String SEND_WARNING_TO_USER = "/sendWarning (\\d+)";
+    private final String GET_HELP = "/catReportHelp";
+    private final String HELP_MESSAGE = "*/getCatReports* - выводит все отчеты из БД\n" +
+            "*/getCatDayReport 2021-01-12* - выводит отчеты из БД за указанную дату\n" +
+            "*/deleteCatsFromReportById 1* - удаляет из БД всех кошек по id = 1(adopted_cat)\n" +
+            "*/deleteCatReportId 1* - удаляет из БД запись с id = 1\n" +
+            "*/getBadUsers 2021-01-12* - выводит список кошек, у которых нет записи в БД на дату\n" +
+            "*/sendWarning 1* - отправляет предупреждение владельцу по id кошки(adopted_cat)\n";
 
+
+    @Command(name = GET_HELP)
+    public SendMessage getHelp(Message message) {
+        return new SendMessage(message.from().id(), HELP_MESSAGE).parseMode(ParseMode.Markdown);
+    }
 
     /**
      * Метод "запоминает" намерения пользователя добавить отчет.
