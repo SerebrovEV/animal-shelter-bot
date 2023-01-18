9e3543b5cfc03de4d3c58f8d361417229562c93d
b.m.ulyashev <b.m.ulyashev@mail.ru>
2023-01-18 18:51:48 +0300

    Сделан контроллер отчетов кошек. Добавлены сервисы и тесты.

---
+++ src/main/java/com/animalshelter/animalshelterbot/controllers/CatReportController.java
@@ -0,0 +1,133 @@
+package com.animalshelter.animalshelterbot.controllers;
+
+import com.animalshelter.animalshelterbot.handler.Callback;
+import com.animalshelter.animalshelterbot.handler.Command;
+import com.animalshelter.animalshelterbot.handler.CommandController;
+import com.animalshelter.animalshelterbot.organisation.Callbacks;
+import com.animalshelter.animalshelterbot.service.CatReportService;
+import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.request.SendMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CatReportController implements CommandController {
+
+    private final CatReportService catReportService;
+
+    private final String PHOTO_PATTERN = "(?U)(\\w+)(\\.)(.+)";
+
+    private final String GET_ALL_CAT_REPORTS = "/getCatReports";
+
+    private final String GET_CAT_DATE_REPORT = "/getCatDayReport (\\d{4}-\\d{2}-\\d{2})";
+    private final String DELETE_CAT_FROM_REPORT_BY_CAT_ID = "/deleteCatsFromReportById ([\\d]+)";
+    private final String DELETE_CAT_FROM_REPORT_BY_ID = "/deleteCatReportId ([\\d]+)";
+    private final String GET_CATS_FROM_BAD_USERS = "/getBadUsers (\\d{4}-\\d{2}-\\d{2})";
+    private final String SEND_WARNING_TO_USER = "/sendWarning (\\d+)";
+
+
+    /**
+     * Метод "запоминает" намерения пользователя добавить отчет.
+     *
+     * @return Пример отчета
+     */
+    @Callback(name = Callbacks.CAT_REPORT)
+    public SendMessage addReportCatCallback(CallbackQuery callback) {
+        return catReportService.addInquiryCatReport(callback);
+    }
+
+    /**
+     * Метод для удаления пользователя из списков на отправку отчета.
+     *
+     * @return {@link SendMessage}
+     */
+    @Callback(name = Callbacks.CAT_ADD_REPORT_NO)
+    public SendMessage deleteUserFromTempReport(CallbackQuery callback) {
+        return catReportService.closeInquiryCatReport(callback);
+    }
+
+    /**
+     * Метод на добавление отчета в БД
+     *
+     * @return {@link SendMessage}
+     */
+    @Callback(name = Callbacks.CAT_ADD_REPORT_YES)
+    public SendMessage createReport(CallbackQuery callback) {
+        return catReportService.addReport(callback);
+    }
+
+    /**
+     * Метод принимает сообщения к фото
+     *
+     * @return {@link SendMessage}
+     */
+    @Command(pattern = PHOTO_PATTERN)
+    public SendMessage cacheReport(Message message) {
+        return catReportService.validateReport(message);
+    }
+
+    /**
+     * Метод на получение всех записей из БД отчетов по кошкам
+     *
+     * @return {@link SendMessage}
+     */
+    @Command(name = GET_ALL_CAT_REPORTS)
+    public SendMessage getReportByToday(Message message) {
+        return catReportService.getAllCatReports(message);
+    }
+
+    /**
+     * Метод на получение всех записей из БД отчетов по кошкам по указанной дате
+     *
+     * @return {@link SendMessage}
+     */
+    @Command(pattern = GET_CAT_DATE_REPORT)
+    public SendMessage getReportByDay(Message message) {
+        return catReportService.getCatReportByDay(message);
+    }
+
+    /**
+     * Удаляет всех кошек из БД отчетов по идентификатору adopted_cat_id.
+     *
+     * @return {@link SendMessage}
+     */
+    @Command(pattern = DELETE_CAT_FROM_REPORT_BY_CAT_ID)
+    public SendMessage deleteCatsFromReportById(Message message) {
+        return catReportService.deleteCatsFromReportByCatId(message);
+    }
+
+    /**
+     * Удаляет запись из БД отчетов.
+     *
+     * @return {@link SendMessage}
+     */
+    @Command(pattern = DELETE_CAT_FROM_REPORT_BY_ID)
+    public SendMessage deleteCatReport(Message message) {
+        return catReportService.deleteCatReport(message);
+    }
+
+    /**
+     * Выводит список питомце, которые не имеют отчетов в указанную дату.
+     *
+     * @return Список питомцев, не имеющих отчет
+     */
+    @Command(pattern = GET_CATS_FROM_BAD_USERS)
+    public List<SendMessage> getCatsWithoutReport(Message message) {
+        return catReportService.getMissingReports(message);
+    }
+
+    /**
+     * Отправляет предупреждение хозяину.
+     *
+     * @param message хранит id усыновленного питомца
+     * @return Список {@link SendMessage}
+     */
+    @Command(pattern = SEND_WARNING_TO_USER)
+    public List<SendMessage> sendWarningToUser(Message message) {
+        return catReportService.sendWarning(message);
+    }
+}
